2a2196afd5692fa906b0a4f358073d0a9c99f317
KRotide <Cheepster38@hotmail.fr>
2023-09-03 20:43:59 +0200

    Installation des dépendances + Réorganisation

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/home";
+import Login from "./pages/login/login";
+import User from "./pages/user/user";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/user" element={<User />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
